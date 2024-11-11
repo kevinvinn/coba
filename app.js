d2c65af41fd5c66ad23ad77248cb94b5a94c2b45
Kevin <kevinrisky18@gmail.com>
2024-11-11 16:46:46 +0700

    add ahay 4

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
 
 app.get("/", (req, res) => {
-  res.send("aku di sini cuy ahay 3");
+  res.send("aku di sini cuy ahay 4");
 });
 
 app.listen(PORT, () => {
